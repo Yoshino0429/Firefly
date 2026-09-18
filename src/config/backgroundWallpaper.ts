c6bbf613de862a7a6d709e10f9a776b4a2653553
Yoshino0429 <203516118+Yoshino0429@users.noreply.github.com>
2026-09-18 19:14:15 +0800

    更换壁纸并开启 Live2D 看板娘

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -107,7 +107,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 				{
 					name: "GitHub",
 					icon: "fa7-brands:github",
-					url: "https://github.com/CuteLeaf/Firefly",
+					url: "https://github.com/Yoshino0429",
 					showName: true,
 				},
 				{
--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "XiaYe",
+	name: "Natsume",
 
 	// 个人签名
-	bio: "Hello, I'm XiaYe.",
+	bio: "キラキラ ドキドキ!",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,13 +24,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/Yoshino0429",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "Yoshino061729@gmail.com",
 			showName: false,
 		},
 		{
