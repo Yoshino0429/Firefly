74c378cbcaea5a96c89943a272d132de1f335479
Yoshino0429 <203516118+Yoshino0429@users.noreply.github.com>
2026-09-19 22:52:33 +0800

    22:52

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -262,7 +262,7 @@ export const siteConfig: SiteConfig = {
 	// ── Bilibili配置 ──────────────────────────────────
 	bilibili: {
 		// 你的 Bilibili 用户 UID
-		uid: "38932988",
+		uid: "423183446",
 	},
 
 	// ── 番组计划bangumi配置 ──────────────────────────────────
